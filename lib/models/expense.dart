2dd9353e35417275be1549c8915212aa21710605
Cherif <cherif.bah.address@gmail.com>
2024-07-16 16:16:52 +0000

    First version of my expensive tracker

---
+++ lib/models/expense.dart
@@ -0,0 +1,21 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+// print(uuid.v4());
+enum Category { food, travel, leisure, work }
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+}
